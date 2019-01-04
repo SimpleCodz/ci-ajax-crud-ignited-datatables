d1e415bab07a510269b898941deb2d65117bd329
SimpleCodz <46218591+SimpleCodz@users.noreply.github.com>
2019-01-04 16:36:17 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ci-apps/ci-crud-ignited-datatable';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
